c33608791718cb25ba18674f963c5a31fccefd3d
1130 <1130@student.itgymnazium.cz>
2025-03-18 14:26:09 +0100

    Add project files.

---
+++ game/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] players = { 10, 10, 10, 10, 10 };
+            double pot = 0;
+            double lastRoundPot = 0;
+
+            Random random = new Random();
+
+            int round = 1;
+            int strategy = 0;
+
+            Console.WriteLine("Everybody has 10 EUR.");
+
+            for (int j = 0; j < 10; j++) //Amount of rounds
+            {
+                Console.WriteLine($"Round {round}");
+                for (int i = 0; i < players.Length; i++)
+                {
+                    double bet = 0;
+
+                    if (round != 1)
+                    {
+                        strategy = random.Next(0, 4);
+                    }
+                    else
+                    {
+                        strategy = random.Next(0, 3);
+                    }
+
+                    string strategyName = string.Empty;
+
+                    if (players[i] == 0)
+                    {
+                        bet = 0;
+                        strategyName = "No money";
+                    }
+                    else if (strategy == 0) //Fair play
+                    {
+                        bet = players[i] / 2;
+                        players[i] -= bet;
+                        strategyName = "Fair play";
+                    }
+                    else if (strategy == 1) //selfish play
+                    {
+                        bet = 0;
+                        strategyName = "Selfish play";
+                    }
+                    else if (strategy == 3) //reactive play
+                    {
+                        bet = lastRoundPot / players.Length;
+                        players[i] -= bet;
+                        strategyName = "Reactive play";
+                    }
+                    else //All in (strategy 2)
+                    {
+                        bet = players[i];
+                        players[i] -= bet;
+                        strategyName = "All in";
+                    }
+
+                        pot += bet;
+
+                    Console.WriteLine($"player {i} betted {bet} EUR and chose the {strategyName} strategy.");
+                }
+
+                
+                Console.WriteLine($"The pot before the koeficient is {pot} EUR");
+                lastRoundPot = pot;
+
+                pot *= 1.5;
+                
+                Console.WriteLine($"The pot after the koeficient is {pot} EUR");
+
+                for (int i = 0; i < players.Length; i++)
+                {
+                    players[i] += pot / players.Length;
+                }
+
+                Console.WriteLine($"player 0 has {players[0]} EUR, player 1 has {players[1]} EUR, player 2 has {players[2]} EUR, player 3 has {players[3]} EUR, player 4 has {players[4]} EUR");
+                Console.WriteLine();
+
+                pot = 0;
+
+                round++;
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,34 @@
+# Simulace Hry
+
+Tento program simuluje sázkovou hru, kde pìt hráèù zaèíná s poèáteèní èástkou 10 EUR kadı. Hra probíhá po dobu 10 kol a v kadém kole si hráèi vybírají strategii pro umístìní svıch sázek. Strategie zahrnují férovou hru, sobeckou hru, reaktivní hru a vše na jednu kartu. Poté je bank vynásoben koeficientem a rovnomìrnì rozdìlen mezi hráèe. Cílem je pozorovat, jak rùzné strategie ovlivòují koneèné èástky hráèù.
+
+## Jak spustit
+
+1. Ujistìte se, e máte nainstalovanı .NET 8.
+2. Otevøete øešení ve Visual Studio 2022.
+3. Sestavte a spuste projekt.
+
+## Strategie
+
+- **Férová hra**: Vsadí polovinu aktuální èástky hráèe.
+- **Sobecká hra**: Nesází nic.
+- **Reaktivní hra**: Vsadí èástku na základì banku z pøedchozího kola.
+- **Vše na jednu kartu**: Vsadí celou èástku hráèe.
+
+### Otázky
+
+#### Funguje to?
+
+Ano, program funguje podle oèekávání. Simuluje sázkovou hru, umoòuje hráèùm vybírat rùzné strategie a aktualizuje jejich èástky po kadém kole.
+
+#### Jak to poznám?
+
+Funkènost programu mùete ovìøit jeho spuštìním a sledováním vıstupu na konzoli. Vıstup zobrazí sázku kadého hráèe, zvolenou strategii, bank pøed a po aplikaci koeficientu a aktualizované èástky hráèù po kadém kole. Konzistentní a logické aktualizace èástek hráèù naznaèují, e program funguje správnì.
+
+### Interpretace
+
+Program ukazuje, jak rùzné sázkové strategie mohou ovlivnit koneèné èástky hráèù bìhem nìkolika kol. Simulací rùznıch strategií poskytuje vhled do úèinnosti jednotlivıch pøístupù v kontrolovaném prostøedí.
+
+### Co jsem si odnesl
+
+Z této simulace jsem se nauèil, e volba strategie vıznamnì ovlivòuje vısledek ve sázkové høe. Strategie jako "Vše na jednu kartu" mohou vést k vysokım odmìnám, ale také k vysokım rizikùm, zatímco "Sobecká hra" zajišuje stabilitu, ale nemusí pøinést vıznamné zisky. Program efektivnì ilustruje dùleitost strategického rozhodování ve hrách náhody.
